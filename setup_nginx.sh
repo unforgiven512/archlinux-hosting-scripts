894c210657f4c26c59c6ebc560a7db8dd27a4e8c
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2014-04-23 23:32:49 -0400

    added some comments to setup_nginx.sh

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-get_cpu_count() {
+
+get_cpu_count() { ## function to get CPU count, and add 1
 	declare -i cpuCount
 	cpuCount=`cat /proc/cpuinfo | grep processor | wc -l`
 	cpuCount=$cpuCount+1
 }
 
-build_nginx_conf() {
+build_nginx_conf() { ## function to build nginx.conf
 	sed -i 's/^worker_processes.*/worker_processes '$cpuCount'\;/' /etc/nginx/nginx.conf
 	
 }
 
-create_folder_structure() {
+create_folder_structure() { ## function to create folder structure for nginx-based hosting
 	mkdir -p "/etc/nginx/vhosts-available"
 	mkdir -p "/etc/nginx/vhosts-enabled"
 	mkdir -p "/etc/nginx/includes"
