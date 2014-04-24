6914a7a5f46e875a1c5677d3b29392aa9f5779fa
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2014-04-24 03:04:33 -0400

    more work on setup_nginx.sh, some more work on server.conf.sample

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source server.defs
+
 
 get_cpu_count() { ## function to get CPU count, and add 1
 	declare -i cpuCount
@@ -8,8 +10,45 @@ get_cpu_count() { ## function to get CPU count, and add 1
 }
 
 build_nginx_conf() { ## function to build nginx.conf
-	sed -i 's/^worker_processes.*/worker_processes '$cpuCount'\;/' /etc/nginx/nginx.conf
-	
+	#sed -i 's/^worker_processes.*/worker_processes '$cpuCount'\;/' /etc/nginx/nginx.conf
+
+cat >> "/etc/nginx/nginx.conf" < EOF
+worker_processes $cpuCount;
+
+events {
+	worker_connections 1024;
+}
+
+http {
+	include			mime.types;
+	default_type		application/octet-stream;
+	sendfile		on;
+	keepalive_timeout	65;
+	gzip			on;
+	gzip_disable		"msie6";
+
+	## includes ##
+	include			/etc/nginx/conf.d/*.conf;
+	include			/etc/nginx/includes/*;
+	include			/etc/nginx/vhosts-enabled/*;
+}
+EOF
+}
+
+create_default_vhost() { ## function to create a default virtual host, and link it to the enabled folder
+cat >> /etc/nginx/vhosts-available/default < EOF
+server {
+  listen 80;
+  server_name ${nServerHostname};
+
+  root /usr/share/nginx/html;
+  index index.html index.htm;
+
+  location / {
+    try_files \$uri \$uri/ /index.html;
+  }
+}
+EOF
 }
 
 create_folder_structure() { ## function to create folder structure for nginx-based hosting
