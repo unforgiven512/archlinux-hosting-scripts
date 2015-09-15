6e6246ac27b101c14cbc58aacb92642994a78c00
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2015-09-15 17:59:20 -0500

    More work on cleaning up; preparation for packaging, and easier development

---
+++ manual_devel_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# manual_devel_install.sh
+# (c) 2015 Gerad Munsch <gmunsch@unforgivendevelopment.com>
+#
+# Perform installation tasks necessary for a "live" development version of "archlinux-hosting-scripts"
+# DO NOT USE UNLESS YOU PLAN ON ACTIVELY DEVELOPING/WORKING ON THIS PACKAGE -- please use the packaged
+# version instead (see README.md; PKGBUILD is present in './arch_pkg_src')
+#
---
+++ src/base_config/etc/archlinux-hosting-scripts/README.md
@@ -0,0 +1,2 @@
+# Configuration #
+This README will detail the various configuration files utilized by the scripts.
