9f530a57be03dc1a4023115eb565c4ca9980492b
Gerad Munsch <gmunsch@unforgivendevelopment.com>
2015-09-19 00:02:43 -0500

    Added base configs for each install type, started some work on the manual installation script.

--- manual_devel_install.sh
+++ manual_devel_install.sh
@@ -7,3 +7,12 @@
 # DO NOT USE UNLESS YOU PLAN ON ACTIVELY DEVELOPING/WORKING ON THIS PACKAGE -- please use the packaged
 # version instead (see README.md; PKGBUILD is present in './arch_pkg_src')
 #
+
+source ./src/base_config/archlinux-hosting-scripts-devel.conf
+
+
+updateGitIgnoreFile ()
+{
+  gitIgnoreFile="${AHS_DEVEL_INSTALL_ROOT_DIR}/.gitignore"
+  
+}
